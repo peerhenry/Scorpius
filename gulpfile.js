6deb93c1aa1fbde7ca2e2cac1c26c1d3d93f486b
PeerHenry <peerhenry@gmail.com>
2016-07-04 19:56:15 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var less = require('gulp-less');
+var livereload = require('gulp-livereload');
+
+gulp.task('pug', function(){
+  return gulp.src(['./src/**/*.pug'])
+  .pipe(pug())
+  .pipe(gulp.dest('./build/'))
+  .pipe(livereload());
+});
+
+gulp.task('less', function(){
+  return gulp.src(['./src/**/*.less'])
+  .pipe(less())
+  .pipe(gulp.dest('./build/'))
+  .pipe(livereload());
+});
+
+gulp.task('livereload-listen', function(){
+  livereload.listen();
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./src/**/*.pug', ['pug']);
+  gulp.watch('./src/**/*.less', ['less']);
+});
+
+gulp.task('default', ['pug', 'less', 'livereload-listen', 'watch']);
